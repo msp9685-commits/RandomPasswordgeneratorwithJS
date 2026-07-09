310945688d1225004d77a76dd73d35b8f6ce9a37
Ayush Singh <msp9685@gmail.com>
2026-07-09 16:04:37 +0530

    Intial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let uppercase = document.querySelector("#uppercase");
+let lowercase = document.querySelector("#lowercase");
+let number = document.querySelector("#number");
+let symbol = document.querySelector("#symbol");
+let button = document.querySelector("#generate");
+
+button.addEventListener("click", function () {
+
+    if (uppercase.checked) {
+        console.log("Uppercase selected");
+    }
+
+});
